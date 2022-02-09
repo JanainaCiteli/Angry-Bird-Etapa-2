b1e37d6809073383f75af344fd7d8c205c481949
Janaina Citeli <citeli@gmail.com>
2022-02-09 13:40:35 -0300

    primeiro

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    plataform = new Ground(160,60,60,60)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    plataform.display();
 }
